e04b7c864d031331b55034e07e1d1f947303d0c1
chefyeum <chefyeum@gmail.com>
2021-07-16 13:30:22 +0100

    add other arithmetic operators

--- generic/FFI/js/div.js
+++ generic/FFI/js/div.js
@@ -1,1 +1,16 @@
-var div = LINKS.kify((x, y) => (BigInt(x) / BigInt(y)));
+
+function bigIntDiv(x, y) {
+    return (BigInt(x) / BigInt(y));
+}
+
+function bigIntAdd(x, y) {
+    return (BigInt(x) + BigInt(y));
+}
+
+function bigIntMult(x, y) {
+    return (BigInt(x) * BigInt(y));
+}
+
+var div = LINKS.kify(bigIntDiv);
+var add = LINKS.kify(bigIntAdd);
+var mult = LINKS.kify(bigIntMult);
